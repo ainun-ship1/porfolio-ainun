09e82d87fccdf93f315506bb734f30c22c9d93f3
ainun-ship1 <ainuncantik0197@gmail.com>
2026-09-24 10:09:49 +0700

    Delete perpus_ainun directory

--- perpus_ainun/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_perpustakaan_ainun";
-$koneksi = mysqli_connect($host, $user, $pass, $db);
-if (!$koneksi){ die("koneksi ke database gagal:" . mysqli_connect_error());
-    }
-    ?>
